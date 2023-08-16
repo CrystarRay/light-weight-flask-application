03bbc572f691ca4f51f148531dd6631a9c44eb40
Chenrui <126846127+CrystarRay@users.noreply.github.com>
2023-08-15 23:55:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,60 @@
+from flask import Flask, request, jsonify
+import requests
 import tempfile
-import time
-
 import cv2
 import mediapipe as mp
 import numpy as np
 import tensorflow as tf
-from flask import Flask, request, jsonify
-from firebase_admin import credentials, initialize_app, storage
-from werkzeug.exceptions import abort
-import requests
 from mutils import convert
 import os
-from google.cloud import storage as gcs
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'mobility-scooter-app-firebase-adminsdk-twji0-182bd76cd8.json'
-os.environ['BUCKET_NAME'] = 'trainingvideo-project-123-20230726'
-
+# Load the model from local directory
 MODEL_NAME = "trained_with_20_files"
-TIMESTAMPS = 16
-bucket_name = os.getenv('BUCKET_NAME')
+model = tf.keras.models.load_model(f"./model/{MODEL_NAME}/model.h5")
 
-# Load your model
-storage_client = gcs.Client()
-bucket = storage_client.bucket(bucket_name)
-blob = bucket.blob(f"model/{MODEL_NAME}/model.h5")
-blob.download_to_filename("/tmp/model.h5")
-model = tf.keras.models.load_model("/tmp/model.h5")
+# Define MediaPipe Pose
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
 
-# Download Firebase credentials to local environment
-firebase_blob = bucket.blob('mobility-scooter-app-firebase-adminsdk-twji0-182bd76cd8.json')
-firebase_blob.download_to_filename('/tmp/mobility-scooter-app-firebase-adminsdk-twji0-182bd76cd8.json')
+# Flask app
+app = Flask(__name__)
+app.debug = True
 
-# Initialize Firebase
-cred = credentials.Certificate('/tmp/mobility-scooter-app-firebase-adminsdk-twji0-182bd76cd8.json')
-default_app = initialize_app(cred, {'storageBucket': bucket_name})
-bucket = storage.bucket()
+@app.route('/', methods=['POST'])
+def predict():
+    url = request.get_json()['url']
+    filename = download_file(url)
 
+    try:
+        # Process video
+        mae_losses = process_video(filename)
+        # Delete the file after processing
+        os.remove(filename)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-def download_file(url, filename):
+    # Return the result
+    if mae_losses:
+        average_loss = sum(mae_losses) / len(mae_losses)
+        result = {"average_loss": average_loss, "all_losses": mae_losses}
+    else:
+        result = "No pose detected"
+
+    return jsonify(result)
+
+def download_file(url):
     r = requests.get(url, stream=True)
-    if r.status_code == 200:
-        with open(filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
+    r.raise_for_status()  # Raise exception if invalid response
+    temp = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4")  # Create a temp file with .mp4 extension
+    with open(temp.name, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            if chunk:
                 f.write(chunk)
-    return filename
-
-def download_file(url, filename):
-    r = requests.get(url, allow_redirects=True)
-    open(filename, 'wb').write(r.content)
+    return temp.name
 
 def process_video(video_file):
     cap = cv2.VideoCapture(video_file)
-    pose = mp.solutions.pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
     mae_losses = []  # Store all the losses
     try:
         while cap.isOpened():
@@ -69,25 +72,5 @@ def process_video(video_file):
         cap.release()
     return mae_losses
 
-app = Flask(__name__)
-
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    data = request.json
-    url = data.get('url')
-
-    video_file = download_file(url, "temp_video_file")
-
-    try:
-        mae_losses = process_video(video_file)
-    except Exception as e:
-        abort(500, description="Failed to process video: " + str(e))
-
-    # Clean up temporary file
-    time.sleep(5)  # add delay
-    os.remove(video_file)
-
-    return jsonify({'mae_losses': mae_losses})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
